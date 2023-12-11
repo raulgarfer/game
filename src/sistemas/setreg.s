3998c40dba0ec762e06cbb9c17d69590bbaf9c7e
raul garcia <raulgarfer@gmail.com>
2023-12-11 19:33:33 +0100

    modified:   src/sistemas/setreg.s

--- src/sistemas/setreg.s
+++ src/sistemas/setreg.s
@@ -1,5 +1,5 @@
 .module setreg
-;;preparamos el crt para pantall de 20*25 tiles
+;;preparamos el crt para pantall de 34*25 tiles
 setreg::
     ld    bc, #0xBC01  ;; [3] B=0xBC CRTC Select Register, C=register number to be selected
         out  (c), c             ;; [4] Select register
